1c29c109951cf5deae278d1ce8eae98c55392870
vasarhelyia <vasarhelyi.agi@gmail.com>
2016-03-09 08:33:20 +0100

    Add package version as input

--- step.sh
+++ step.sh
@@ -1,1 +1,13 @@
-npm install -g ${npm_options} react-native-cli
+#!/bin/bash
+
+set -e
+
+VERSION=""
+
+echo "Available react-native-cli versions: $(npm view react-native-cli versions)"
+
+if [ ! -z "${version}" ] ; then
+    VERSION="@${version}"
+fi
+
+npm install -g ${npm_options} react-native-cli${VERSION}
